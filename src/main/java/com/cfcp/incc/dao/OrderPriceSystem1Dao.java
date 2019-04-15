3d77b64e2832ec4f8b750da9034fda020a3d1e95
1529747001@qq.com <1529747001@qq.com>
2019-04-15 18:30:39 +0800

    价格体系  新

---
+++ src/main/java/com/cfcp/incc/dao/OrderPriceSystem1Dao.java
@@ -0,0 +1,16 @@
+package com.cfcp.incc.dao;
+
+
+import com.cfcp.incc.entity.OrderPriceSystem;
+import com.cfcp.incc.entity.OrderPriceSystem1;
+
+import java.util.List;
+import java.util.Map;
+
+public interface OrderPriceSystem1Dao {
+
+    List<OrderPriceSystem1> queryAll1_1(Map conditions);
+    List<OrderPriceSystem1> queryAll1();
+    int insert(OrderPriceSystem1 op);
+    int update(OrderPriceSystem1 op);
+}
